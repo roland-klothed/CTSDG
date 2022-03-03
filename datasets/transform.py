d5e94e2504017716d0c348291498bce27981fc0d
roland-klothed <99288058+roland-klothed@users.noreply.github.com>
2022-03-03 11:07:48 -0800

    Update transform.py

--- datasets/transform.py
+++ datasets/transform.py
@@ -7,7 +7,7 @@ def image_transforms(load_size):
 
     return transforms.Compose([
         # transforms.CenterCrop(size=(178, 178)),  # for CelebA
-        transforms.Resize(size=load_size, interpolation=Image.BILINEAR),   
+        transforms.RandomCrop(size=load_size),   
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
